48e820ae9c15a05706ce1c95956b3583ded22f56
StanislavPB <perminovsb@gmail.com>
2025-09-22 12:32:22 +0100

    add: Validation

--- src/main/java/org/demointernetshop0505/service/FileInfoService.java
+++ src/main/java/org/demointernetshop0505/service/FileInfoService.java
@@ -1,4 +1,21 @@
 package org.demointernetshop0505.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
 public class FileInfoService {
+
+
+
+    public String upload(MultipartFile file) throws IOException {
+
+
+
+        return "Файл успешно сохранен";
+    }
 }
--- src/main/java/org/demointernetshop0505/service/UserService.java
+++ src/main/java/org/demointernetshop0505/service/UserService.java
@@ -1,10 +1,10 @@
 package org.demointernetshop0505.service;
 
 import jakarta.transaction.Transactional;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.demointernetshop0505.dto.UserRequestDto;
 import org.demointernetshop0505.dto.UserResponseDto;
+import org.demointernetshop0505.dto.UserUpdateRequestDto;
 import org.demointernetshop0505.entity.User;
 import org.demointernetshop0505.repository.UserRepository;
 import org.demointernetshop0505.service.exception.AlreadyExistException;
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Data
 @RequiredArgsConstructor
 @Service
 public class UserService{
@@ -73,5 +72,93 @@ public class UserService{
 
         return converter.toDto(user);
     }
+
+
+    @Transactional
+    public UserResponseDto confirmationEmail(String code){
+
+        User user = codeConfirmationService.changeConfirmationStatusByCode(code);
+
+        user.setStatus(User.Status.CONFIRMED);
+
+        repository.save(user);
+
+        return converter.toDto(user);
+    }
+
+
+    @Transactional
+    public UserResponseDto updateUser(UserUpdateRequestDto updateRequest){
+
+        if (updateRequest.getEmail() == null || updateRequest.getEmail().isBlank()){
+            throw new IllegalArgumentException("Email must be provided to update user");
+        }
+
+        String userEmail = updateRequest.getEmail();
+
+        // найдем пользователя по email
+        User userByEmail = repository.findByEmail(userEmail)
+                .orElseThrow(() -> new NotFoundException("User with email: " + userEmail + " not found"));
+
+        // обновляем все доступные поля
+        // мы заранее НЕ ЗНАЕМ, а какие именно поля пользователь захочет поменять
+        // то есть в JSON (в теде запроса) будут находится ТОЛЬКО те поля (со значением)
+        // которые пользователь хочет менять (не обязательно все)
+        if (updateRequest.getFirstName() != null && !updateRequest.getFirstName().isBlank()) {
+            userByEmail.setFirstName(updateRequest.getFirstName());
+        }
+
+        if (updateRequest.getLastName() != null && !updateRequest.getLastName().isBlank()) {
+            userByEmail.setLastName(updateRequest.getLastName());
+        }
+
+        if (updateRequest.getHashPassword() != null && !updateRequest.getHashPassword().isBlank()) {
+            userByEmail.setHashPassword(updateRequest.getHashPassword());
+        }
+
+        // сохраняем (обновляем) пользователя
+        repository.save(userByEmail);
+
+        return converter.toDto(userByEmail);
+        // или вручную создать UserResponseDto из данных, которые хранятся в userByEmail
+    };
+
+    @Transactional
+    public boolean deleteUser(Integer id){
+
+        // проверим, что такой id существует
+        // и если нет - то сразу возвращаем false и ничего даже не пытаемся удалить
+
+        if (!repository.existsById(id)){
+            return false;
+        }
+
+        // если существует, то
+        // вариант 1 - удаляем сразу по id
+
+        repository.deleteById(id);
+
+        // вариант 2 - сперва найдем объект по этому номеру id
+
+//        User userForDelete = repository.findById(id).get();
+//
+//        repository.delete(userForDelete);
+
+        return true;
+
+    }
+
+
+    @Transactional
+    public boolean renewCode(String email){
+
+        User user = repository.findByEmail(email)
+                .orElseThrow(() -> new NotFoundException("User with email: " + email + " not found"));
+
+        codeConfirmationService.confirmationCodeManager(user);
+        return true;
+    }
+
+
 
 }
