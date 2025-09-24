6a903b4e998f025b2631556c17048043a8c8b123
StanislavPB <perminovsb@gmail.com>
2025-09-24 09:33:45 +0100

    add: controller

---
+++ src/main/java/org/demointernetshop0505/controller/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+package org.demointernetshop0505.controller;
+
+import jakarta.validation.ConstraintViolationException;
+import org.demointernetshop0505.service.exception.AlreadyExistException;
+import org.demointernetshop0505.service.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.format.DateTimeParseException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<String> handlerDateTimeParseException(DateTimeParseException e){
+        return new ResponseEntity<>( e.getMessage() , HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handlerNullPointerException(NullPointerException e){
+        return new ResponseEntity<>( e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handlerNotFoundException(NotFoundException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(AlreadyExistException.class)
+    public ResponseEntity<String> handlerAlreadyExistException(AlreadyExistException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    }
+
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handlerConstraintViolationException(ConstraintViolationException e){
+        StringBuilder responseMessage = new StringBuilder();
+
+        e.getConstraintViolations().forEach(
+                constraintViolation -> {
+                    String currentField = constraintViolation.getPropertyPath().toString();
+                    String currentMessage = constraintViolation.getMessage();
+                    responseMessage.append("В поле: " + currentField + " : " + currentMessage);
+                    responseMessage.append("\n");
+                }
+        );
+        return new ResponseEntity<>(responseMessage.toString(), HttpStatus.BAD_REQUEST);
+    }
+
+
+}
